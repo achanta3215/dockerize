2aef8c3565d88ca3a477b806f04e66cd857156b1
Krishna Sameer Achanta <achanta3215@gmail.com>
2024-04-11 11:56:46 +0530

    Update script to copy SSH key on host

--- virtualize/rockylinux/start.sh
+++ virtualize/rockylinux/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ -d ".vagrant" ]; then
-    echo ".vagrant directory exists."
+if [[ $1 == "--init" ]]; then
+    echo "Initializing"
+    vagrant up
+    vagrant scp ~/.ssh/id_ed25519.pub /home/vagrant/.ssh
+    vagrant ssh -t -- 'sudo dnf update -y'
+    vagrant halt
     vagrant up --provision
 # Perform actions if the directory exists
 else
-    echo ".vagrant directory does not exist."
-    vagrant up
-    vagrant ssh -t -- 'sudo dnf update -y'
-    vagrant halt
     vagrant up --provision
 # Perform actions if the directory doesn't exist
 fi
