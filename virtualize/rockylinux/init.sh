083080b2c30f694b2469b8ca550b384b63474faa
Krishna Sameer Achanta <achanta3215@gmail.com>
2024-07-26 10:55:42 +0530

    Update init.sh for rockylinux

--- virtualize/rockylinux/init.sh
+++ virtualize/rockylinux/init.sh
@@ -13,7 +13,7 @@ pip3 install podman-compose
 sudo dnf install -y podman-docker
 ### Docker Installation
 # sudo dnf -y install dnf-plugins-core
-# sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+# sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 # sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.26.1/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
